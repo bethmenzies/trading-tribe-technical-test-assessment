c612f844a091035330eea91ce45e92d20abb3e15
bethmenzies <bethmenzies@btinternet.com>
2021-06-09 21:51:46 +0100

    getAllFixtures with necessary assertions

--- cypress/integration/getAllFixtures.spec.js
+++ cypress/integration/getAllFixtures.spec.js
@@ -2,6 +2,20 @@ import fixtures from '../../data/fixtures'
 
 describe('get all fixtures', () => {
     it('should get all fixtures', () => {
-        cy.request('/fixtures').its('body').should('deep.equal', fixtures)
+        cy.request('/fixtures').as('fixtures')
+
+        // this will make sure that the /fixtures response deeply equals the data you provided
+        cy.get('@fixtures').its('body').should('deep.equal', fixtures)
+
+        // below I'll also add the more in depth checks
+        cy.get('@fixtures').should((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body).to.have.length(3)
+            response.body.forEach((fixture) => {
+                console.log(fixture)
+                expect(fixture).to.have.property('fixtureId')
+                assert.isString(fixture.fixtureId)
+            })
+        })
     })
 })
