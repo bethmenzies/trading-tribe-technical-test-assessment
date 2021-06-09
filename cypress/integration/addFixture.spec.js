389adac096eba41493eb1a52f762abc678c47136
bethmenzies <bethmenzies@btinternet.com>
2021-06-09 22:55:17 +0100

    solution with cy.wait for a minute

--- cypress/integration/addFixture.spec.js
+++ cypress/integration/addFixture.spec.js
@@ -1,20 +1,42 @@
 describe('add a fixture', () => {
     it('can add a new fixture', () => {
+        cy.intercept('GET', '/fixtures').as('allFixtures')
+
         cy.fixture('fixture').then((json) => {
             cy.request('POST', '/fixture', json)
+        })
+
+        cy.wait(60000)
+        cy.request('/fixture/4').as('newFixture')
+
+        cy.get('@newFixture').should((response) => {
+            expect(response.status).be.equal(200)
+            expect(response.body.footballFullState).to.have.property('teams')
+            expect(response.body.footballFullState.teams[0]).to.have.property('teamId', 'HOME')
+        })
+    })
+
+    it('can update the new fixture', () => {
+        cy.fixture('fixtureUpdate').then((json) => {
+            cy.request('PUT', '/fixture', json)
         })
 
         cy.request('/fixture/4').as('newFixture')
 
         cy.get('@newFixture').should((response) => {
             expect(response.status).be.equal(200)
-            expect(response.body).to.have.property('teams')
-            expect(response.body.teams).to.be.instanceOf(Array)
-            expect(response.body.teams[0]).to.have.property('teamId', 'HOME')
+            expect(response.body.footballFullState).to.have.property('goals')
+            expect(response.body.footballFullState.goals).to.have.length(2)
+            expect(response.body.footballFullState.goals[1]).to.have.property('clockTime', 5260)
         })
     })
 
-    after(() => {
+    it('can delete the new fixture', () => {
         cy.request('DELETE', '/fixture/4')
+
+        cy.request('/fixtures').should((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body).to.have.length(3)
+        })
     })
 })
